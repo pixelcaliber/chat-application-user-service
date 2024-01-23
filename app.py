0cedca23345ef19cb4ea45672252d3c4d66beb5c
pixelcaliber <abhinavpandey0032@gmail.com>
2024-01-23 17:27:30 +0530

    add isort and black to format code and refactoring

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, request, jsonify, json
-from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (JWTManager, create_access_token,
+                                get_jwt_identity, jwt_required)
+from flask_login import UserMixin
 from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
-from utils import JWT_ACCESS_TOKEN_EXPIRES, JWT_SECRET_KEY, FLASK_SECRET_KEY, POSTGRES_DB_URL
+from flask_sqlalchemy import SQLAlchemy
+
+from utils import (FLASK_SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES, JWT_SECRET_KEY,
+                   POSTGRES_DB_URL)
 
 app = Flask(__name__)
-app.config['FLASK_SECRET_KEY'] = FLASK_SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_DB_URL
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_COOKIE_SECURE'] = True
-app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
-
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from flask_bcrypt import Bcrypt
+app.config["FLASK_SECRET_KEY"] = FLASK_SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = POSTGRES_DB_URL
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SESSION_COOKIE_SECURE"] = True
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = JWT_ACCESS_TOKEN_EXPIRES
 
 bcrypt = Bcrypt(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-jwt = JWTManager(app)
+jwt = JWTManager(app) 
 
-from flask_login import UserMixin
-from datetime import datetime
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,65 +34,78 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.cli.command('db_create')
+
+@app.cli.command("db_create")
 def db_create():
     db.create_all()
-    print('Database created!')
+    print("Database created!")
 
-@app.cli.command('db_drop')
+
+@app.cli.command("db_drop")
 def db_drop():
     db.drop_all()
-    print('Database dropped!')
+    print("Database dropped!")
 
-@app.cli.command('db_seed')
+
+@app.cli.command("db_seed")
 def db_seed():
-    user = User(username='admin', email='admin@example.com', password='admin_password')
+    user = User(username="admin", email="admin@example.com", password="admin_password")
     db.session.add(user)
     db.session.commit()
-    print('Database seeded!')
+    print("Database seeded!")
 
-@app.route('/register', methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    username = data['username']
+    username = data["username"]
 
     # Check if username and email are not already taken
-    existing_user = User.query.filter((User.username == data['username']) | (User.email == data['email'])).first()
+    existing_user = User.query.filter(
+        (User.username == data["username"]) | (User.email == data["email"])
+    ).first()
     if existing_user:
-        return jsonify({'error': 'Username or email already exists'}), 400
+        return jsonify({"error": "Username or email already exists"}), 400
 
     # Hash the password before saving to the database
-    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    hashed_password = bcrypt.generate_password_hash(data["password"]).decode("utf-8")
 
     # Create a new user
-    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    new_user = User(
+        username=data["username"], email=data["email"], password=hashed_password
+    )
     db.session.add(new_user)
     db.session.commit()
 
     # Generate access token for the new user
-    access_token = create_access_token(identity={'username': new_user.username, 'email': new_user.email})
-    response = f'User: {username} is successfully registered with the access_token: {access_token}'
+    access_token = create_access_token(
+        identity={"username": new_user.username, "email": new_user.email}
+    )
+    response = f"User: {username} is successfully registered with the access_token: {access_token}"
     return {"Success": response}, 201
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
-    username = data['username']
+    username = data["username"]
     user = User.query.filter_by(username=username).first()
 
-    if user and bcrypt.check_password_hash(user.password, data['password']):
-        access_token = create_access_token(identity={'username': user.username, 'email': user.email})
-        response = f'User: {username} is successfully logged in with the access_token: {access_token}'
+    if user and bcrypt.check_password_hash(user.password, data["password"]):
+        access_token = create_access_token(
+            identity={"username": user.username, "email": user.email}
+        )
+        response = f"User: {username} is successfully logged in with the access_token: {access_token}"
         return {"Success": response}, 200
+    return {"error": "Invalid username or password"}, 401
 
-    return {'error': 'Invalid username or password'}, 401
 
-@app.route('/protected', methods=['GET'])
+@app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
-    return {'logged_in_as': current_user}, 200
+    return {"logged_in_as": current_user}, 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
