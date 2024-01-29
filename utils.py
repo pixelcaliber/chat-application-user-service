c41a78ec17facf9e12863ca5e15a354cbbc1ec4f
pixelcaliber <abhinavpandey0032@gmail.com>
2024-01-29 16:49:54 +0530

    add Kafka to create events upon new user registration and lru cache for frequent chat_id retrievals

---
+++ utils.py
@@ -0,0 +1,16 @@
+import os
+from datetime import timedelta
+
+FLASK_SECRET_KEY = os.environ.get("FLASK_SECRET_KEY")
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=int(15))
+DB_NAME = os.environ.get("DB_NAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+
+POSTGRES_DB_URL = (
+    "postgresql://"
+    + str(DB_NAME)
+    + ":"
+    + str(DB_PASSWORD)
+    + "@localhost/chat-app-user-db"
+)
