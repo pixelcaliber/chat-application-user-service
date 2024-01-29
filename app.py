ab8bb1acfcbc03bcdc9140c1821111e3f982c77b
Abhinav Pandey <79640544+pixelcaliber@users.noreply.github.com>
2024-01-29 17:12:08 +0530

    add kafka for event notification of new registered user and lru for retrieval of frequently used chat_ids

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import logging
 import uuid
 from datetime import datetime
+from functools import lru_cache
 
-from flask import Flask, jsonify, request
+from confluent_kafka import Producer
+from flask import Flask, json, jsonify, request
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import (JWTManager, create_access_token,
                                 get_jwt_identity, jwt_required)
@@ -12,6 +15,8 @@ from sqlalchemy.dialects.postgresql import UUID
 
 from utils import (FLASK_SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES, JWT_SECRET_KEY,
                    POSTGRES_DB_URL)
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 app.config["FLASK_SECRET_KEY"] = FLASK_SECRET_KEY
@@ -28,6 +33,11 @@ migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 
+# Kafka producer setup
+producer_config = {"bootstrap.servers": "localhost:9092"}
+producer = Producer(producer_config)
+
+
 class User(db.Model, UserMixin):
     # id = db.Column(db.Integer, primary_key=True)
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -37,16 +47,22 @@ class User(db.Model, UserMixin):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
+class users_active(db.Model, UserMixin):
+    userid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    active = db.Column(db.Boolean, nullable=False)
+
+
 @app.cli.command("db_create")
 def db_create():
     db.create_all()
-    print("Database created!")
+    logging.info("Database created!")
 
 
 @app.cli.command("db_drop")
 def db_drop():
     db.drop_all()
-    print("Database dropped!")
+    logging.info("Database dropped!")
 
 
 @app.cli.command("db_seed")
@@ -59,7 +75,10 @@ def db_seed():
     )
     db.session.add(new_user)
     db.session.commit()
-    print("Database seeded!")
+    logging.info("Database seeded!")
+
+
+# python -m flask db_seed
 
 
 @app.route("/register", methods=["POST"])
@@ -87,12 +106,27 @@ def register():
     db.session.add(new_user)
     db.session.commit()
 
+    new_user_state = users_active(userid=user_id, username=username, active=True)
+    db.session.add(new_user_state)
+    db.session.commit()
+
     # Generate access token for the new user
     access_token = create_access_token(
-        identity={"username": new_user.username, "email": new_user.email, "user_id": new_user.id}
+        identity={
+            "username": new_user.username,
+            "email": new_user.email,
+            "user_id": new_user.id,
+        }
     )
     response = {"username": username, "user_id": user_id, "access_token": access_token}
+    kafka_message = {
+        "header": f"Welcome to chat application: {username}",
+        "body": "Good to see you!",
+    }
+    producer.produce("received_messages", value=json.dumps(kafka_message))
+    producer.flush()
     return jsonify(response), 201
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -102,9 +136,17 @@ def login():
 
     if user and bcrypt.check_password_hash(user.password, data["password"]):
         access_token = create_access_token(
-            identity={"username": user.username, "email": user.email, "user_id": user.id}
+            identity={
+                "username": user.username,
+                "email": user.email,
+                "user_id": user.id,
+            }
         )
-        response = {"username": username, "user_id": user.id, "access_token": access_token}
+        response = {
+            "username": username,
+            "user_id": user.id,
+            "access_token": access_token,
+        }
         return jsonify(response), 200
     return {"error": "Invalid username or password"}, 401
 
@@ -116,9 +158,19 @@ def protected():
     return {"logged_in_as": current_user}, 200
 
 
-@app.route("/users/<string:username>", methods=["GET"])
+@lru_cache(maxsize=128)
+@app.route("/users/username/<string:username>", methods=["GET"])
 def get_user_by_username(username):
     user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify({"username": user.username, "user_id": user.id}), 200
+    return jsonify({"message": "User not found"}), 404
+
+
+@lru_cache(maxsize=128)
+@app.route("/users/user_id/<string:user_id>", methods=["GET"])
+def get_user_by_user_id(user_id):
+    user = User.query.filter_by(id=user_id).first()
     if user:
         return jsonify({"username": user.username, "user_id": user.id}), 200
     return jsonify({"message": "User not found"}), 404
